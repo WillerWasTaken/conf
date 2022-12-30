dc476e9f76779ab8d618cceaedb27a50905c8eb4
William Chhim <william@join-jump.com>
2022-12-30 14:57:36 +0100

    fix(lock): override screen file

--- scripts/usr/local/bin/lock.sh
+++ scripts/usr/local/bin/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scrot /tmp/screen.png
+scrot -o /tmp/screen.png
 #convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 #convert /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
 convert /tmp/screen.png -paint 3 /tmp/screen.png
