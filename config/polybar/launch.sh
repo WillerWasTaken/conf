705b122091bf85e4d1a9d1e4b7e32c735453afaf
William Chhim <william@join-jump.com>
2023-08-25 09:30:30 +0200

    feat(polybar): auto detect interfaces and use pkill

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+pkill polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+wifi=$(echo /sys/class/net/*/wireless | awk -F'/' '{ print $5 }')
+eth=$(ls /sys/class/net | grep ^e)
+WIRELESS_INTERFACE=$wifi ETHERNET_INTERFACE=$eth polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
