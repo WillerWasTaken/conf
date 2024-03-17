e89494f4a3b57f3ef9ec3d8c56bcd2368ac4984d
William Chhim <ly.william.chhim@gmail.com>
2024-03-17 22:06:26 +0100

    feat(nvim,treesitter): remove netwr and window picker

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -4,7 +4,33 @@ return {
   keys = {
     { "<leader>o", ":NvimTreeFindFile<CR>", desc = "Tree toogle" }
   },
+  lazy = false,
   config = function()
-    require("nvim-tree").setup()
+    -- Completely disable builtin netrw in favor of nvim tree
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    require("nvim-tree").setup({
+      on_attach = function(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        vim.keymap.set('n', 'CD', api.tree.change_root_to_node, opts('CD'))
+      end,
+      actions = {
+        open_file = {
+          window_picker = {
+            -- Remove picker for open and split
+            enable = false,
+          },
+        },
+      }
+    })
   end
 }
