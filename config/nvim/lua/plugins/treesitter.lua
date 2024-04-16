8b8a2ea53563ddc97c5eaa6cf05a12b3317f95e0
William Chhim <ly.william.chhim@gmail.com>
2024-04-16 23:59:14 +0200

    feat(nvim): add textobjects manipulation

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = "<cmd>TSUpdate",
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
@@ -24,6 +26,47 @@ return {
         "terraform",
         "yaml",
         "vimdoc"
+      },
+      -- Consistent syntax highlighting
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["as"] = "@assignment.outer",
+            ["is"] = "@assignment.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+              ["<C-Down>"] = "@function.outer",
+          },
+          goto_previous_start = {
+              ["<C-Up>"] = "@function.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_previous = {
+              ["<leader>pl"] = "@parameter.inner",
+          },
+          swap_next = {
+              ["<leader>pr"] = "@parameter.inner",
+          },
+        },
       },
     })
   end
