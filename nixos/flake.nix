b88a05e33f0eacdf01c79f7676791cfb7fdb4980
William Chhim <ly.william.chhim@gmail.com>
2024-12-13 18:02:55 +0100

    feat(nix): upgrade to 24.11

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -13,7 +13,7 @@
   outputs = { self, nixpkgs, ... }@inputs:
     let
       systemConfiguration = {
-        nixVersion = "24.05";
+        nixVersion = "24.11";
         hostname = "nixos";
         system = "x86_64-linux";
         timeZone = "Europe/Paris";
