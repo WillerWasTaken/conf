7fbb03652b8d8b9a76d7b7d3ad2688778b4769b4
William Chhim <ly.william.chhim@gmail.com>
2025-03-11 10:01:20 +0100

    feat(nvim): revert leap behavior on scrollback

--- config/nvim/lua/plugins/leap.lua
+++ config/nvim/lua/plugins/leap.lua
@@ -2,11 +2,5 @@ return {
   "ggandor/leap.nvim",
   config = function()
     require("leap").create_default_mappings()
-
-    -- Invert the default on scrollback as the cursor will be at the bottom
-    if vim.env.KITTY_SCROLLBACK_NVIM == 'true' then
-        vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-forward)')
-        vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-backward)')
-    end
   end
 }
