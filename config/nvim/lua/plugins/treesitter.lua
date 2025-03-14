08afaf53af005642eb80b595740d083fb0534c25
William Chhim <ly.william.chhim@gmail.com>
2025-03-14 11:06:52 +0100

    fix(nvim,treesitter): fix cmd no such file or directory

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = "<cmd>TSUpdate",
+  build = ":TSUpdate",
   dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 
   config = function()
