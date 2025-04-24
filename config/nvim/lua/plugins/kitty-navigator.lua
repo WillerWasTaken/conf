4f4632c458b793819c892c079bb1243c15c6dbb0
William Chhim <ly.william.chhim@gmail.com>
2025-04-24 16:02:03 +0200

    refactor(nvim): remove unused kitty navigator setting

--- config/nvim/lua/plugins/kitty-navigator.lua
+++ config/nvim/lua/plugins/kitty-navigator.lua
@@ -1,9 +1,6 @@
 return {
   "knubie/vim-kitty-navigator",
   lazy = false,
-  config = function()
-    vim.g.kitty_navigator_enable_stack_layout = 1
-  end,
   keys = {
     {"<C-h>", function() vim.cmd("KittyNavigateLeft") end, desc = "Move left a Split", mode = {"n"}},
     {"<C-j>", function() vim.cmd("KittyNavigateDown") end, desc = "Move down a Split", mode = {"n"}},
