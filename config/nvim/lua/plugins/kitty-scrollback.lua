cffbe2737b1d8ba2b83704e4722602bf7344a27e
William Chhim <ly.william.chhim@gmail.com>
2025-04-24 14:06:27 +0200

    refactor(nvim): remove redundant setup call

--- config/nvim/lua/plugins/kitty-scrollback.lua
+++ config/nvim/lua/plugins/kitty-scrollback.lua
@@ -1,8 +1,5 @@
 return {
   "mikesmithgh/kitty-scrollback.nvim",
   cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
-  event = { "User KittyScrollbackLaunch" },
-  config = function()
-    require("kitty-scrollback").setup()
-  end,
+  event = { "User KittyScrollbackLaunch" }
 }
