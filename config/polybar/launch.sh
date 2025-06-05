feb6e91420d08d3a1c7c1c4b97208836cf7edaa5
William Chhim <ly.william.chhim@gmail.com>
2025-06-05 14:46:54 +0200

    feat(polybar): add playerctl and update icons

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -2,11 +2,11 @@
 
 # Terminate already running bar instances
 pkill polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
+echo "---" | tee /tmp/polybar1.log
 wifi=$(echo /sys/class/net/*/wireless | awk -F'/' '{ print $5 }')
 eth=$(ls /sys/class/net | grep ^e)
 WIRELESS_INTERFACE=$wifi ETHERNET_INTERFACE=$eth polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
--- nixos/home.nix
+++ nixos/home.nix
@@ -154,6 +154,7 @@ in {
   services.network-manager-applet.enable = true;
   services.blueman-applet.enable = true;
   services.picom.enable = true;
+  services.playerctld.enable = true;
 
   home.file = {
     ".vimrc".source = dotfileSymlink "vimrc";
