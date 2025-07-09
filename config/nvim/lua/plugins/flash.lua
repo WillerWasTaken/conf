57a41f3c299e9e8d4a725556ff230eaa1a786933
William Chhim <ly.william.chhim@gmail.com>
2025-07-09 13:59:25 +0200

    feat(nvim): replace leap with flash

---
+++ config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      char = {
+        -- disable f, t enhancement
+        enabled = false,
+      },
+    }
+  },
+  -- stylua: ignore
+  keys = function()
+    local flash = require("flash")
+    return {
+      { "s", mode = { "n", "x", "o" }, function() flash.jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() flash.treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() flash.remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() flash.treesitter_search() end, desc = "Treesitter Search" },
+    }
+  end
+}
