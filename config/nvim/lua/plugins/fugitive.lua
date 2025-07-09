d53071ded3e9e7101d3cc146de05213ca00c66f3
William Chhim <ly.william.chhim@gmail.com>
2025-07-09 18:25:44 +0200

    feat(nvim): trigger gitgutter on save

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,17 @@
 return {
   "tpope/vim-fugitive",
-  dependencies = { "airblade/vim-gitgutter" },
+  dependencies = {
+    {
+      "airblade/vim-gitgutter",
+      init = function ()
+        -- Update signs as soon as save is performed to avoid the delay
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+          pattern = {"*"},
+          callback = function() vim.cmd("GitGutter") end,
+        })
+      end
+    },
+  },
   keys = {
     { "<leader>g", function() vim.cmd("G") end, desc = "Toggle fugitive", mode = {"n"}}
   },
