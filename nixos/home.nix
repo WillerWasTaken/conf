1f25fbb77ce2b52f51cedb8a12b75df67938fa54
William Chhim <ly.william.chhim@gmail.com>
2025-07-16 18:06:06 +0200

    refactor(nix): move home, services and programs in dedicated blocks

--- nixos/home.nix
+++ nixos/home.nix
@@ -7,10 +7,6 @@ let
   configSymlink = path: mkMutableSymlink homeConfiguration.configDir path;
   assetsSymlink = path: mkMutableSymlink homeConfiguration.assetsDir path;
 in {
-  home.username = homeConfiguration.username;
-  home.homeDirectory = homeConfiguration.homeDir;
-
-  home.stateVersion = nixVersion;
   nix = {
     gc = {
       automatic = true;
@@ -22,124 +18,153 @@ in {
   xsession = {
     enable = true;
   };
-  home.keyboard = {
-    layout = homeConfiguration.kbLayout;
-    variant = homeConfiguration.kbVariant;
-  };
 
   # Enable font discovery through home manager
   fonts.fontconfig.enable = true;
 
-  home.packages = with pkgs; [
-    kitty
-    python3
-    asdf-vm
-    zplug
+  home = {
+    username = homeConfiguration.username;
+    homeDirectory = homeConfiguration.homeDir;
 
-    # Cmd
-    wget
-    gnumake
-    unzip
-    tldr
-    dig
-    openssl
-    imagemagick
-    scrot
-    xsel
-    tree
-    yq-go
-    tcpdump
-    ipcalc
-    ncdu
-    magic-wormhole
-    wireguard-tools
+    stateVersion = nixVersion;
+    keyboard = {
+      layout = homeConfiguration.kbLayout;
+      variant = homeConfiguration.kbVariant;
+    };
 
-    # Devops
-    terraform
-    terraform-docs
-    krew
-    kubectl
-    kubectx
-    kconf
-    kubernetes-helm
-    kind
+    packages = with pkgs; [
+      kitty
+      python3
+      asdf-vm
+      zplug
 
-    # Language
-    cargo
-    cargo-watch
-    rustc
-    clang
+      # Cmd
+      wget
+      gnumake
+      unzip
+      tldr
+      dig
+      openssl
+      imagemagick
+      scrot
+      xsel
+      tree
+      yq-go
+      tcpdump
+      ipcalc
+      ncdu
+      magic-wormhole
+      wireguard-tools
 
-    # Azure
-    kubelogin
-    azure-cli
-    azuredatastudio
+      # Devops
+      terraform
+      terraform-docs
+      krew
+      kubectl
+      kubectx
+      kconf
+      kubernetes-helm
+      kind
 
-    # Desktop
-    i3lock-color
-    pavucontrol
-    pamixer
-    arandr
-    polybarFull
-    dunst
+      # Language
+      cargo
+      cargo-watch
+      rustc
+      clang
 
-    # Graphical
-    youtube-music
-    unstable.rambox
-    signal-desktop
-    keepassxc
-    flameshot
-    pdfarranger
-  ] ++ builtins.filter pkgs.lib.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+      # Azure
+      kubelogin
+      azure-cli
+      azuredatastudio
 
-  programs.autorandr.enable = true;
-  programs.bat.enable = true;
-  programs.direnv.enable = true;
-  programs.firefox.enable = true;
-  programs.chromium.enable = true;
-  programs.fzf.enable = true;
-  programs.git = {
-    enable = true;
-    package = pkgs.gitFull;
+      # Desktop
+      i3lock-color
+      pavucontrol
+      pamixer
+      arandr
+      polybarFull
+      dunst
+
+      # Graphical
+      youtube-music
+      unstable.rambox
+      signal-desktop
+      keepassxc
+      flameshot
+      pdfarranger
+    ] ++ builtins.filter pkgs.lib.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
+    file = {
+      ".vimrc".source = dotfileSymlink "vimrc";
+      ".p10k.zsh".source = dotfileSymlink "p10k.zsh";
+      ".gitconfig".source = dotfileSymlink "gitconfig";
+      ".zshrc".source = dotfileSymlink "zshrc";
+      ".kubectl_aliases".source = builtins.fetchurl {
+        url = https://raw.githubusercontent.com/ahmetb/kubectl-aliases/refs/heads/master/.kubectl_aliases;
+      };
+
+      ".config/kitty/kitty.conf".source = configSymlink "kitty/kitty.conf";
+      ".config/kitty/current-theme.conf".source = configSymlink "kitty/current-theme.conf";
+      ".config/i3/config".source = configSymlink "i3/config";
+      ".config/polybar".source = configSymlink "polybar";
+      ".config/dunst".source = configSymlink "dunst";
+      ".config/nvim".source = configSymlink "nvim";
+
+      ".background-image".source = assetsSymlink "hou-china-6.jpg";
+      ".lock.png".source = assetsSymlink "rick_and_morty_lock.png";
+    };
   };
-  programs.go.enable = true;
-  programs.htop.enable = true;
-  programs.jq.enable = true;
-  programs.k9s.enable = true;
-  # Conflict with kitty conf file
-  # programs.kitty.enable = true;
-  programs.ripgrep.enable = true;
-  programs.rofi = {
-    enable = true;
-    theme = "solarized_alternate";
-  };
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    extraPackages = with pkgs; [
-      # LSP
-      ansible-language-server
-      nodePackages.bash-language-server
-      docker-compose-language-service
-      dockerfile-language-server-nodejs
-      gopls
-      jq-lsp
-      vscode-langservers-extracted
-      lua-language-server
-      nginx-language-server
-      python3Packages.python-lsp-server
-      rust-analyzer
-      terraform-ls
-    ];
-  };
-  # Conflict with .ssh/config
-  # programs.ssh.enable = true;
-  programs.zsh = {
-    enable = true;
-    sessionVariables = {
-      ZPLUG_DIR = "${pkgs.zplug}/share/zplug";
-      FZF_DIR = "${config.programs.fzf.package}/share/fzf";
-      ASDF_DIR = "${pkgs.asdf-vm}/share/asdf-vm";
+
+  programs = {
+    autorandr.enable = true;
+    bat.enable = true;
+    direnv.enable = true;
+    firefox.enable = true;
+    chromium.enable = true;
+    fzf.enable = true;
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+    };
+    go.enable = true;
+    htop.enable = true;
+    jq.enable = true;
+    k9s.enable = true;
+    # Conflict with kitty conf file
+    # kitty.enable = true;
+    ripgrep.enable = true;
+    rofi = {
+      enable = true;
+      theme = "solarized_alternate";
+    };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      extraPackages = with pkgs; [
+        # LSP
+        ansible-language-server
+        nodePackages.bash-language-server
+        docker-compose-language-service
+        dockerfile-language-server-nodejs
+        gopls
+        jq-lsp
+        vscode-langservers-extracted
+        lua-language-server
+        nginx-language-server
+        python3Packages.python-lsp-server
+        rust-analyzer
+        terraform-ls
+      ];
+    };
+    # Conflict with .ssh/config
+    # ssh.enable = true;
+    zsh = {
+      enable = true;
+      sessionVariables = {
+        ZPLUG_DIR = "${pkgs.zplug}/share/zplug";
+        FZF_DIR = "${config.programs.fzf.package}/share/fzf";
+        ASDF_DIR = "${pkgs.asdf-vm}/share/asdf-vm";
+      };
     };
   };
 
@@ -152,29 +177,11 @@ in {
   #   };
   # };
 
-  services.network-manager-applet.enable = true;
-  services.blueman-applet.enable = true;
-  services.picom.enable = true;
-  services.playerctld.enable = true;
-
-  home.file = {
-    ".vimrc".source = dotfileSymlink "vimrc";
-    ".p10k.zsh".source = dotfileSymlink "p10k.zsh";
-    ".gitconfig".source = dotfileSymlink "gitconfig";
-    ".zshrc".source = dotfileSymlink "zshrc";
-    ".kubectl_aliases".source = builtins.fetchurl {
-      url = https://raw.githubusercontent.com/ahmetb/kubectl-aliases/refs/heads/master/.kubectl_aliases;
-    };
-
-    ".config/kitty/kitty.conf".source = configSymlink "kitty/kitty.conf";
-    ".config/kitty/current-theme.conf".source = configSymlink "kitty/current-theme.conf";
-    ".config/i3/config".source = configSymlink "i3/config";
-    ".config/polybar".source = configSymlink "polybar";
-    ".config/dunst".source = configSymlink "dunst";
-    ".config/nvim".source = configSymlink "nvim";
-
-    ".background-image".source = assetsSymlink "hou-china-6.jpg";
-    ".lock.png".source = assetsSymlink "rick_and_morty_lock.png";
+  services = {
+    network-manager-applet.enable = true;
+    blueman-applet.enable = true;
+    picom.enable = true;
+    playerctld.enable = true;
   };
 
   programs.home-manager.enable = true;
