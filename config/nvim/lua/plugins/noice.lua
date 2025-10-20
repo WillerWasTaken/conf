e44cb834f1df3c1d7dd086cadc394cb3dccbed2c
William Chhim <ly.william.chhim@gmail.com>
2025-10-20 17:01:36 +0200

    feat(noice): display long messages as popups

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -2,6 +2,24 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
+    routes = {
+      {
+        -- Long messages as popup
+        filter = {
+          event = "msg_show",
+          min_height = 50,
+        },
+        view = "popup",
+      },
+    },
+    views = {
+      popup = {
+        size = {
+          width = "90%",
+          height = "90%",
+        },
+      },
+    },
   },
   dependencies = {
     "MunifTanjim/nui.nvim",
