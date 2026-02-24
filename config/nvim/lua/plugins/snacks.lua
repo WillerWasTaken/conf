7e6a6d83887d59e362cd923894cf0880c9f4f9bc
William Chhim <ly.william.chhim@gmail.com>
2026-02-24 09:48:47 +0100

    feat: set lazygit workflow

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -5,5 +5,17 @@ return {
     input = {
       enabled = true,
     },
-  }
+    -- https://github.com/folke/snacks.nvim/blob/main/docs/lazygit.md
+    lazygit = {
+      enabled = true,
+    }
+  },
+  keys = function()
+    local snacks = require("snacks")
+    return {
+      { "<leader>gg", snacks.lazygit.open, desc = "Open lazy git" },
+      { "<leader>gl", snacks.lazygit.log, desc = "Open git log" },
+      { "<leader>gf", snacks.lazygit.log_file, desc = "Open git log of current file" }
+    }
+  end
 }
