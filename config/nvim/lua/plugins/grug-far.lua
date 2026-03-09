ddee15b2a01829bb067e951bff01aa8d8ca74828
William Chhim <ly.william.chhim@gmail.com>
2026-03-09 14:17:30 +0100

    feat(vim): more natural bindings

--- config/nvim/lua/plugins/grug-far.lua
+++ config/nvim/lua/plugins/grug-far.lua
@@ -6,6 +6,8 @@ return {
     keymaps = {
       close = { n = "q" },
       qflist = { n = "<C-q>" },
+      openNextLocation = { n = "<C-n>" },
+      openPrevLocation = { n = '<C-p>' },
     },
   },
   keys = function()
@@ -14,4 +16,19 @@ return {
       { "<leader>s", mode = { "n", "x" }, grug_far.open, desc = "Toggle GrugFar" },
     }
   end,
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "grug-far",
+      callback = function(ev)
+        -- Defer the call to wait grugfar to first set the buggy mappings
+        vim.schedule(function()
+          -- restore normal p/P behavior so register-prefixed paste works (e.g. "+p)
+          vim.api.nvim_buf_del_keymap(ev.buf, "n", "p")
+          vim.api.nvim_buf_del_keymap(ev.buf, "v", "p")
+          vim.api.nvim_buf_del_keymap(ev.buf, "n", "P")
+          vim.api.nvim_buf_del_keymap(ev.buf, "v", "P")
+        end)
+      end,
+    })
+  end
 }
