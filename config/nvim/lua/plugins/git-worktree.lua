eea5ecda008435b157c3475254c552e0c568853a
William Chhim <ly.william.chhim@gmail.com>
2026-04-03 09:48:28 +0200

    feat(nvim): clean branch and remote on worktree deletion

--- config/nvim/lua/plugins/git-worktree.lua
+++ config/nvim/lua/plugins/git-worktree.lua
@@ -40,5 +40,33 @@ return {
   end,
   config = function()
     require("telescope").load_extension("git_worktree")
+
+    local git_worktree = require("git-worktree")
+    git_worktree.on_tree_change(function(op, metadata)
+      if op == git_worktree.Operations.Delete then
+        local get_git_root = function()
+          return vim.fn.trim(vim.fn.system("git rev-parse --git-common-dir"))
+        end
+
+        -- deduce the branch name using relative path of the worktree
+        local branch = metadata.path:sub(#get_git_root() + 2)
+        vim.notify("Deleting branch " .. branch, vim.log.levels.INFO)
+
+        local delete_branch = vim.fn.system("git branch --delete " .. branch)
+
+        if vim.v.shell_error ~= 0 then
+          vim.notify("Failed to delete branch: " .. delete_branch, vim.log.levels.ERROR)
+          return
+        end
+        vim.notify("Branch deleted " .. branch, vim.log.levels.INFO)
+
+        local fetch_prune = vim.fn.system("git fetch --prune")
+        if vim.v.shell_error ~= 0 then
+          vim.notify("Failed to fetch: " .. fetch_prune, vim.log.levels.ERROR)
+          return
+        end
+        vim.notify(fetch_prune, vim.log.levels.INFO)
+      end
+    end)
   end,
 }
