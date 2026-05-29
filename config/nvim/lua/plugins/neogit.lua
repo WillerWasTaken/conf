d28c6f6cb5214f15eb4c69cc2523ea805efc2246
William Chhim <ly.william.chhim@gmail.com>
2026-05-29 15:21:33 +0200

    feat(nvim): use default neogit windows style

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -25,39 +25,6 @@ return {
   },
   cmd = "Neogit",
   opts = {
-    commit_editor = {
-      kind = "floating",
-    },
-    commit_select_view = {
-      kind = "floating",
-    },
-    commit_view = {
-      kind = "floating",
-    },
-    log_view = {
-      kind = "floating",
-    },
-    rebase_editor = {
-      kind = "floating",
-    },
-    reflog_view = {
-      kind = "floating",
-    },
-    merge_editor = {
-      kind = "floating",
-    },
-    preview_buffer = {
-      kind = "floating",
-    },
-    popup = {
-      kind = "floating",
-    },
-    stash = {
-      kind = "floating",
-    },
-    refs_view = {
-      kind = "floating",
-    },
     prompt_amend_commit = false,
     mappings = {
       status = {
@@ -69,8 +36,8 @@ return {
   keys = function()
     local neogit = require("neogit")
     return {
-      { "<leader>gg", function() neogit.open({ kind = "floating" }) end, desc = "Show Neogit UI" },
-      { "<leader>gd", function() vim.cmd("CodeDiff") end,                desc = "Toggle codediff" }
+      { "<leader>gg", neogit.open,                        desc = "Show Neogit UI" },
+      { "<leader>gd", function() vim.cmd("CodeDiff") end, desc = "Toggle codediff" }
     }
   end
 }
