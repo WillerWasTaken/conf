6342e8949dbc68d8444de024657d707a7fa7d111
William Chhim <ly.william.chhim@gmail.com>
2026-08-30 10:43:00 +0200

    refactor(nix): use solely udiskie for device mounting

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,8 +71,6 @@
   };
 
   # Enable device mounting
-  services.devmon.enable = true;
-  services.gvfs.enable = true;
   services.udisks2.enable = true;
 
   # Hyprland (Wayland)
--- nixos/home.nix
+++ nixos/home.nix
@@ -299,6 +299,17 @@ in {
       };
     };
     podman.enable = true;
+    udiskie = {
+      enable = true;
+      settings = {
+          # workaround for
+          # https://github.com/nix-community/home-manager/issues/632
+          program_options = {
+              # replace with your favorite file manager
+              file_manager = "${pkgs.thunar}/bin/thunar";
+          };
+      };
+    };
   };
 
   programs.home-manager.enable = true;
